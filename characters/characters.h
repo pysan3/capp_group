396d6359c5992b5f946528f243c4f4595a029958
山本圭乃 <yoshinoyamamoto@yamamotokeinonoMacBook-puro.local>
2020-07-08 22:52:22 +0900

    commit

--- characters/characters.h
+++ characters/characters.h
@@ -1,24 +1,21 @@
 #pragma once
 
-#include "../header.h"
-
 void draw_doraemon(void);
-void draw_LOTSO(void);
+
 typedef enum {
     DORAEMON,
-    LOTSO,
 } Character;
+
+#include "../header.h"
 
 void draw_character(Character c) {
     switch (c) {
         case DORAEMON:
             draw_doraemon();
             break;
-        case LOTSO:
-            draw_LOTSO();
-            break;
         default:
             break;
     }
 }
+
 
