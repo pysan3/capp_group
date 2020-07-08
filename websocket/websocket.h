325c1901b90b1d28347e8f4f27cb77f20109359a
takuto <takutoitoi914@ruri.waseda.jp>
2020-07-08 13:41:09 +0900

    updated websocket.h

--- websocket/websocket.h
+++ websocket/websocket.h
@@ -9,8 +9,8 @@
 // 戻り値が-1の場合fail、エラーを吐いて終了すること
 int ws_init(void);
 
-// 玉が当たってHPが減少したことを他のプレイヤーに周知する
-// 与えられたidのPlayerのhpが変わったことを全員に伝える関数
-// 引数: id: Player.id, hp: Player.hp
+// プレイヤーのデータ更新をサーバに送信
+// 各パラメータを更新したあとに呼び出すこと
+// 引数: p: Playerのポインタ
 // 戻り値: 失敗で-1
-int ws_sendHP(Player);
+int ws_sendPlayer(Player *p);
