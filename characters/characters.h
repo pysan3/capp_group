4b1953fdfb305632c3cb65bd5d3a362180c9678f
firaito <firaito@gmail.com>
2020-07-14 06:28:39 +0900

    miffy（ドラえもん）追加
    miffyといいながらドラえもんが表示されるmiffy.c

--- characters/characters.h
+++ characters/characters.h
@@ -3,11 +3,13 @@
 typedef enum {
     DORAEMON,
     LOTSO,
+    MIFFY,
     // ここ
 } Character;
 
 void draw_doraemon(void);
 void draw_LOTSO(void);
+void draw_miffy(void);
 // ここに追加
 
 void draw_character(Character c);
