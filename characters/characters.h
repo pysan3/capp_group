079bb09e1bde739ee1ad584926f1a1ff34008d87
Kondo-Tomomi <cherry_tmm@fuji.waseda.jp>
2020-07-13 14:44:30 +0900

    最初のコミット

--- characters/characters.h
+++ characters/characters.h
@@ -3,11 +3,13 @@
 typedef enum {
     DORAEMON,
     LOTSO,
+    RILAKKUMA
     // ここ
 } Character;
 
 void draw_doraemon(void);
 void draw_LOTSO(void);
+void draw_Rilakkuma(void);
 // ここに追加
 
 void draw_character(Character c);
