e01390102f1fd1603a93862bd3026128be935e2d
Kondo-Tomomi <cherry_tmm@fuji.waseda.jp>
2020-07-13 15:01:51 +0900

    character.c has changed

--- characters/characters.c
+++ characters/characters.c
@@ -8,6 +8,9 @@ void draw_character(Character c) {
         case LOTSO:
             draw_LOTSO();
             break;
+        case RILAKKUMA:
+            draw_Rilakkuma();
+            break;
         // case ここ:
         //     draw_ここ();
         //     break;
