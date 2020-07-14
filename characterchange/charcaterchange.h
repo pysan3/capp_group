b067ee21818472338a022a8d44de9ec9464661ab
firaito <firaito@gmail.com>
2020-07-14 10:22:09 +0900

    関数変更
    関数変更しました

--- characterchange/charcaterchange.h
+++ characterchange/charcaterchange.h
@@ -3,15 +3,10 @@
 #include "../header.h"
 
 //Player meを生成
-void cc_makeme()
+void cc_makeme(&fieldinfo);
 
-//Player enemiesを生成
-void cc_makeenemy()
-
-//char *name を生成する
-char *name()
 
 //キャラ選択画面の表示
-void cc_window()
+void cc_window();
 
 
