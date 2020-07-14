5a72dbaf256c42025789ca2fc9705c9f0950ce53
firaito <firaito@gmail.com>
2020-07-14 09:19:55 +0900

    miffy削除しました
    miffy削除しました

--- characters/characters.h
+++ characters/characters.h
@@ -3,13 +3,12 @@
 typedef enum {
     DORAEMON,
     LOTSO,
-    MIFFY,
     // ここ
 } Character;
 
 void draw_doraemon(void);
 void draw_LOTSO(void);
-void draw_miffy(void);
+
 // ここに追加
 
 void draw_character(Character c);
