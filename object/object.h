3c27136264fa69decc3bf78fbb478b2854aa82cb
山本圭乃 <yoshinoyamamoto@yamamotokeinonoMacBook-puro.local>
2020-07-20 13:25:51 +0900

    object.cを描きました。

--- object/object.h
+++ object/object.h
@@ -8,8 +8,9 @@ void draw_ground(void);
 // 雪だるまを描画する
 // 引数:locationが雪だるまの座標
 // y=0が地面とする。
-void draw_snowman(Coordinate location);
+void draw_snowman(Coordinate *location);
 
 // 玉を描画する
 // 引数:locationが玉の中心座標
-void draw_bullet(Coordinate location);
+void draw_bullet(Coordinate *location);
+
