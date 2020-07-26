2fb2bf7e84fc75767248a2f89320c1f7bc731b69
山本圭乃 <yoshinoyamamoto@yamamotokeinonoMacBook-puro.local>
2020-07-26 17:50:34 +0900

    calc Normalを削除しました。

--- object/object.c
+++ object/object.c
@@ -18,24 +18,6 @@ GLfloat color[][4] = {
 	{0.2, 0.2, 0.25, 0.7}
 };
 
-void calcNormal(GLdouble v0[3], GLdouble v1[3], GLdouble v2[3], GLdouble n[3])
-{
-	GLdouble v2v[2][3];
-	GLdouble vt[3];
-	double abs;
-	int i;
-	for (i = 0; i < 3; i++)
-	{
-		v2v[0][i] = v1[i] - v0[i];
-		v2v[1][i] = v2[i] - v0[i];
-	}
-	vt[0] = v2v[0][1] * v2v[1][2] - v2v[0][2] * v2v[1][1];
-	vt[1] = v2v[0][2] * v2v[1][0] - v2v[0][0] * v2v[1][2];
-	vt[2] = v2v[0][0] * v2v[1][1] - v2v[0][1] * v2v[1][0];
-	abs = sqrt(vt[0] * vt[0] + vt[1] * vt[1] + vt[2] * vt[2]);
-	for (i = 0; i < 3; i++)
-		n[i] = vt[i] / abs;
-}
 
 void draw_ground(void){
 	
